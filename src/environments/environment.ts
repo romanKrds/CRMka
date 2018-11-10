8dfdd75d76a63e536af3d94b489a815733b266aa
romanKrds <roman.kordas7@gmail.com>
2018-11-10 20:32:16 +0200

    add project sckeleton

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD15QXAPH10jgSTA1FY9COUAlm7LSE6y_U",
+    authDomain: "crmka-5641d.firebaseapp.com",
+    databaseURL: "https://crmka-5641d.firebaseio.com",
+    projectId: "crmka-5641d",
+    storageBucket: "crmka-5641d.appspot.com",
+    messagingSenderId: "876020124131"
+  }
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
