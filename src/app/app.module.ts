b20b427165f51a371796867b11b6c68bec72a68a
romanKrds <roman.kordas7@gmail.com>
2018-11-11 14:07:15 +0200

    angularfire dependancy fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+// import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { MaterialModules } from './materials.module';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './store/reducers';
@@ -19,7 +20,8 @@ import { AppEffects } from './store/effects/app.effects';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule, // for database
+    // AngularFireDatabaseModule, // for database
+    AngularFireDatabaseModule,
     MaterialModules,
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([AppEffects]),
