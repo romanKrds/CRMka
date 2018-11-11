15fd8b4949a45658b4ac11682eeeb323d83fe983
Leo <leo.baltazor@gmail.com>
2018-11-12 01:18:06 +0200

    added services reducers

---
+++ src/app/models/services-store.models.ts
@@ -0,0 +1,6 @@
+import { Services } from '.';
+import { EntityState } from '@ngrx/entity';
+
+export interface StateServices extends EntityState<Services> {
+  // additional entities state properties
+}
