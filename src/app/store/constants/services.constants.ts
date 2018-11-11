3601062762c00a589426bcc0ca32660604fd193d
Leo <leo.baltazor@gmail.com>
2018-11-12 01:16:46 +0200

    added Services.constants

---
+++ src/app/store/constants/services.constants.ts
@@ -0,0 +1,13 @@
+export enum ServicesActionTypes {
+  GetServices = '[Services] Get Servicess',
+  LoadServicess = '[Services] Load Servicess',
+  AddServices = '[Services] Add Services',
+  UpsertServices = '[Services] Upsert Services',
+  AddServicess = '[Services] Add Servicess',
+  UpsertServicess = '[Services] Upsert Servicess',
+  UpdateServices = '[Services] Update Services',
+  UpdateServicess = '[Services] Update Servicess',
+  DeleteServices = '[Services] Delete Services',
+  DeleteServicess = '[Services] Delete Servicess',
+  ClearServicess = '[Services] Clear Servicess'
+}
