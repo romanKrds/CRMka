52dff18814327e7969b498e396e4a03b0c97be2b
Leo <leo.baltazor@gmail.com>
2018-11-12 01:16:10 +0200

    added interface StateServices

--- src/app/models/app-store.model.ts
+++ src/app/models/app-store.model.ts
@@ -1,9 +1,11 @@
+import { StateServices } from './services-store.models';
+
 // Todo check for elvis operator. Is required?
 export interface AppStore {
   currentClient?: string;
   currentBusiness?: string;
   orders?: any;
   customers?: any;
-  services?: any;
+  services?: StateServices;
   states?: any;
 }
---
+++ src/app/models/services.model.ts
@@ -0,0 +1,6 @@
+export interface Services {
+  id: string;
+  businessId: string;
+  description: string;
+  title: string;
+}
