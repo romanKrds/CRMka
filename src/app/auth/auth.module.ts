8bd342629e7689bce8e710fffeebef14144df80e
Leo <leo.baltazor@gmail.com>
2018-11-12 23:19:38 +0200

    init auth

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ],
+  declarations: [LoginComponent, RegisterComponent, UserComponent]
+})
+export class AuthModule { }
--- src/app/models/app-store.model.ts
+++ src/app/models/app-store.model.ts
@@ -1,7 +1,9 @@
 import { StateServices } from './services-store.models';
+import { State } from '../store/reducers/user.reducer';
 
 // Todo check for elvis operator. Is required?
 export interface AppStore {
+  user: State;
   currentClient?: string;
   currentBusiness?: string;
   orders?: any;
