8c9b6436537002b4e5e17f24f4abcb0931154998
Leo <leo.baltazor@gmail.com>
2018-11-12 22:56:22 +0200

    init

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -8,10 +8,13 @@ import {
 import { environment } from '../../../environments/environment';
 import { AppStore } from 'src/app/models';
 import { reducerServices } from './services.reducer';
+import * as fromUser from './user.reducer';
 
 export const reducers: ActionReducerMap<AppStore> = {
 
   services: reducerServices,
+
+  user: fromUser.reducer,
 };
 
 
