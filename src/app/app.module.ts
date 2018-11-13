c133c2e80978aef07d219325c86378f2c8462192
egor066416 <066416@gmail.com>
2018-11-13 21:08:56 +0200

    orders effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppEffects } from './store/effects/app.effects';
 import { ServiceEffects } from './store/effects/service.effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
+// import * as fromOrder from './order.reducer';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { AppRoutingModule } from './app-routing.module';
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([AppEffects, ServiceEffects]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    // StoreModule.forFeature('order', fromOrder.reducer),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -5,3 +5,4 @@ import { Actions, Effect } from '@ngrx/effects';
 export class AppEffects {
   constructor(private actions$: Actions) {}
 }
+
