1dae2f8e066061e4e874a770fc93e5a82ba3ed2c
egor066416 <066416@gmail.com>
2018-11-13 22:40:22 +0200

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppEffects } from './store/effects/app.effects';
 import { ServiceEffects } from './store/effects/service.effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
+import { OrdersEffects } from './store/effects/orders.effects';
 // import * as fromOrder from './order.reducer';
 
 @NgModule({
@@ -28,7 +29,7 @@ import { AppRoutingModule } from './app-routing.module';
     AngularFireDatabaseModule,
     MaterialModules,
     StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([AppEffects, ServiceEffects]),
+    EffectsModule.forRoot([AppEffects, ServiceEffects, OrdersEffects]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     // StoreModule.forFeature('order', fromOrder.reducer),
   ],
