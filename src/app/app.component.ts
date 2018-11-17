967593fcec02b678568683ee6c51f89740c32f3b
Roman_Kordas <roman.kordas7@gmail.com>
2018-11-17 13:56:58 +0200

    tweaks after merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Store } from '@ngrx/store';
 
-import { AppStore, Order, Orders, Status } from '@models/index';
+import { AppStore, Orders, } from '@models/index';
 import { GetStatuses } from './store/actions/statuses.actions';
 import { GetOrders } from './store/actions/order.actions';
 import { GetServices } from './store/actions/services.actions';
 import { selectStateServices } from './store/selectors/services.selectors';
-import { selectStateStatuses, selectAllStatuses, selectStatusesAsArray } from './store/selectors/statuses.selectors';
+import { selectStatusesAsArray } from './store/selectors/statuses.selectors';
 
 
 @Component({
@@ -17,33 +17,32 @@ import { selectStateStatuses, selectAllStatuses, selectStatusesAsArray } from '.
 })
 
 export class AppComponent implements OnInit {
-  title = 'CRMka';
-  
-  constructor(private db: AngularFireDatabase, private state: Store<AppStore>) {}
+
+  constructor(private db: AngularFireDatabase, private store: Store<AppStore>) {}
 
   ngOnInit() {
     this.store.dispatch(new GetOrders());
-    this.state.dispatch(new GetStatuses());
-    this.state.dispatch(new GetServices());
+    this.store.dispatch(new GetStatuses());
+    this.store.dispatch(new GetServices());
 
     this.store.select('orders').subscribe(
-      (orders: Orders) => this.orders = orders
+      (orders: Orders) => console.log(orders)
     );
-    
-    this.state
+
+    this.store
       .select(selectStatusesAsArray)
       .subscribe( statuses => {
           console.log('STATUSES: ', statuses);
           // this.statuses = statuses;
         }
-      )
+      );
 
-    this.state
+    this.store
       .select(selectStateServices)
       .subscribe(value => console.log('SERVICES: ', value));
     this.db
       .list('/clients')
       .valueChanges()
-      .subscribe(value => console.log('CLIENTS: ',value));
+      .subscribe(value => console.log('CLIENTS: ', value));
   }
 }
