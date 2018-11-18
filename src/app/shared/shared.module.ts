eb7429c48eb7081b910af4abd268a74605f13056
romanKrds <roman.kordas7@gmail.com>
2018-11-18 12:57:02 +0200

    add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../materials.module';
+
+const MODULES = [
+  CommonModule,
+  ReactiveFormsModule,
+  MaterialModule
+];
+
+@NgModule({
+  imports: [
+    ...MODULES
+  ],
+  declarations: []
+})
+export class SharedModule { }
