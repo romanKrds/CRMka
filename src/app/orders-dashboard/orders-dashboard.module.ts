66faac35ff9d74b02913ecd9daf0b2c0f8d44d8e
egor066416 <066416@gmail.com>
2018-11-18 13:09:48 +0200

    adding filter panel

--- src/app/orders-dashboard/orders-dashboard.module.ts
+++ src/app/orders-dashboard/orders-dashboard.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersDashboardRoutingModule } from './orders-dashboard-routing.module';
 import { OrdersDashboardComponent } from './orders-dashboard.component';
+import { FilterPanelComponent } from './filter-panel/filter-panel.component';
+import { MaterialModules } from '../materials.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModules,
     OrdersDashboardRoutingModule
   ],
-  declarations: [OrdersDashboardComponent]
+  declarations: [OrdersDashboardComponent, FilterPanelComponent]
 })
 export class OrdersDashboardModule { }
