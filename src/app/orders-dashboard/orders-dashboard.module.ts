4a13153a4c7918744b3e57f3884affae8bfae3fc
egor066416 <066416@gmail.com>
2018-11-18 13:00:25 +0200

    add product card component

--- src/app/orders-dashboard/orders-dashboard.module.ts
+++ src/app/orders-dashboard/orders-dashboard.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersDashboardRoutingModule } from './orders-dashboard-routing.module';
 import { OrdersDashboardComponent } from './orders-dashboard.component';
+import { PoductCardComponent } from './poduct-card/poduct-card.component';
 
 @NgModule({
   imports: [
     CommonModule,
     OrdersDashboardRoutingModule
   ],
-  declarations: [OrdersDashboardComponent]
+  declarations: [OrdersDashboardComponent, PoductCardComponent]
 })
 export class OrdersDashboardModule { }
