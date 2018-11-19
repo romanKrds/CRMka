64346a3d93f0ab5d682f9464951dc0079ba4ef44
Khavkin <hdm@ukr.net>
2018-11-20 00:05:07 +0200

    added loading data for selects.

--- src/app/orders-dashboard/filter-panel/filter-panel.component.ts
+++ src/app/orders-dashboard/filter-panel/filter-panel.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Status, Service, AppStore, StatusesState } from '@models/*';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { selectStatusesAsArray, selectServicesAll } from '@selectors/*';
 
 
 @Component({
@@ -8,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterPanelComponent implements OnInit {
 
-  constructor() { }
+  services: Service[] = [];
+  statuses: Status[] = [];
+
+  @Input() isOrderFilter: boolean;
+
+
+  constructor(private store: Store<AppStore>) { }
 
   ngOnInit() {
+    if (this.isOrderFilter) {
+      this.loadDataForSelects();
+    }
+    // console.log(this);
+  }
+
+  loadDataForSelects(): void {
+    this.store.select(selectStatusesAsArray)
+    .subscribe(
+      (statuses: Status[] ) => this.statuses = statuses);
+
+    this.store.select(selectServicesAll)
+      .subscribe(
+        (services: any) => services.map(key => this.services = [key, ...this.services])
+      );
+
   }
 
 }
