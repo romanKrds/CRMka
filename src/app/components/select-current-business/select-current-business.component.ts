a7fd222fd8c55cb571e84713d0d96179a1fe6689
Leo <leo.baltazor@gmail.com>
2018-11-28 20:44:05 +0200

    init

---
+++ src/app/components/select-current-business/select-current-business.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-current-business',
+  templateUrl: './select-current-business.component.html',
+  styleUrls: ['./select-current-business.component.scss']
+})
+export class SelectCurrentBusinessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
