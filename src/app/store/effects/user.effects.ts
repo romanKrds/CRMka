bab9ecacf6e0644db3a9e53aac7fac51dcf3e030
Leo <leo.baltazor@gmail.com>
2018-11-28 02:31:18 +0200

    update to 7

--- src/app/store/effects/user.effects.ts
+++ src/app/store/effects/user.effects.ts
@@ -3,19 +3,24 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { from, Observable, of, defer } from 'rxjs';
-import { catchError, map, switchMap, mergeMap, tap, pluck } from 'rxjs/operators';
+import {
+  catchError,
+  map,
+  switchMap,
+  mergeMap,
+  tap,
+  pluck,
+  concatMap
+} from 'rxjs/operators';
 import { UserActionTypes } from '@constants/*';
-import { auth, UserInfo } from 'firebase/app';
 import {
   LoadServices,
   GetUser,
   Authenticated,
   NotAuthenticated,
   AuthError,
-  GoogleLogin,
   PasswordLogin,
   Logout,
-  LoadUserData,
   LoadStatuses,
   LoadOrders,
   LoadCustomers,
@@ -27,11 +32,8 @@ import {
   ClearStatuses
 } from '@actions/*';
 import { AngularFireDatabase } from '@angular/fire/database';
-// import { User, } from '@models/*';
 import { Router } from '@angular/router';
 import { User } from 'firebase/app';
-import { UserState } from '@models/*';
-
 
 @Injectable()
 export class AuthEffects {
@@ -45,6 +47,7 @@ export class AuthEffects {
   @Effect()
   getUser: Observable<Action> = this.actions$.pipe(
     ofType(UserActionTypes.GetUser),
+    map((action: GetUser) => action),
     switchMap(_ => this.afAuth.authState),
     switchMap((authData: User) => {
       if (authData) {
@@ -65,94 +68,92 @@ export class AuthEffects {
           last_name = null;
           displayName = 'NoName';
         }
-      const currentUser = {
-        first_name: first_name,
-        last_name: last_name,
-        displayName: displayName,
-        email: authData.email,
-        phoneNumber: authData.phoneNumber,
-        photoURL: authData.photoURL,
-        providerId: authData.providerId,
-        uid: authData.uid
-      };
-      const userListRef = this.db.object('/clients/' + authData.uid);
-      userListRef.update(currentUser);
-      return userListRef.valueChanges();
-    }
+        const currentUser = {
+          first_name: first_name,
+          last_name: last_name,
+          displayName: displayName,
+          email: authData.email,
+          phoneNumber: authData.phoneNumber,
+          photoURL: authData.photoURL,
+          providerId: authData.providerId,
+          uid: authData.uid
+        };
+        const userListRef = this.db.object('/clients/' + authData.uid);
+        userListRef.update(currentUser);
+        return userListRef.valueChanges();
+      }
       return of(null);
     }),
-    switchMap((user: UserState) => {
+    map(user => {
       if (user) {
-        return from([
-          new Authenticated({ user }),
-          new LoadStatuses(),
-          new LoadOrders(),
-          new LoadServices(),
-          new LoadCustomers(),
-          new LoadBusiness()
-        ]);
+        return new Authenticated({ user });
       } else {
-        // this.router.navigate(['']);
-        return from([
-          new NotAuthenticated(),
-          new ClearStatuses(),
-          new ClearOrders(),
-          new ClearServices(),
-          new ClearCustomers(),
-          new ClearBusinesses()
-        ]);
+        return new NotAuthenticated();
       }
     }),
     catchError(err => of(new AuthError({ error: err.message })))
   );
 
-  @Effect({dispatch: false})
+  @Effect({ dispatch: false })
   authenticated$ = this.actions$.pipe(
     ofType<Authenticated>(UserActionTypes.Authenticated),
+    tap(_ => {
+      return from([
+        new LoadStatuses(),
+        new LoadOrders(),
+        new LoadServices(),
+        new LoadCustomers(),
+        new LoadBusiness()
+      ]);
+    }),
     tap(_ => this.router.navigate(['base'])),
     catchError(err => {
       console.log('authenticatedError', err);
       return err;
     })
   );
-  @Effect({dispatch: false})
-  notAuthenticated$ = this.actions$.pipe((
-    ofType<NotAuthenticated>(UserActionTypes.NotAuthenticated),
+  @Effect({ dispatch: false })
+  notAuthenticated$ = this.actions$.pipe(
+    (ofType<NotAuthenticated>(UserActionTypes.NotAuthenticated),
     tap(_ => {
-      this.router.navigate(['login']);
+      return from([
+        new ClearStatuses(),
+        new ClearOrders(),
+        new ClearServices(),
+        new ClearCustomers(),
+        new ClearBusinesses()
+      ]);
     }),
+    tap(_ => this.router.navigate(['login'])),
     catchError(err => {
       console.log('notAuthenticatedError', err);
       return err;
-    })
-  ));
-
-  @Effect({dispatch: false})
-  loginPassword = this.actions$.pipe(
-    ofType<PasswordLogin>(UserActionTypes.EmailPasswordLogin),
-    pluck('payload'),
-    tap(loginData => {
-      return this.signInWithEmailAndPassword(loginData);
-    }),
-    catchError(err => {
-      return of(new AuthError({ error: err.message }));
-  }));
-
+    }))
+  );
   @Effect({ dispatch: false })
   logout: Observable<Action> = this.actions$.pipe(
     ofType(UserActionTypes.Logout),
-    map((action: Logout) => {
-      return action.payload;
-    }),
+    map((action: Logout) => action.payload),
     switchMap(_ => {
       return of(this.afAuth.auth.signOut());
     }),
     map(_ => {
       return new NotAuthenticated();
     }),
-    catchError(err => {
-      return of(new AuthError({ error: err.message }));
-  })
+    catchError(error => {
+      return of(new AuthError({ error }));
+    })
+  );
+
+  @Effect()
+  loginPassword: Observable<Action> = this.actions$.pipe(
+    ofType<PasswordLogin>(UserActionTypes.EmailPasswordLogin),
+    map((action: PasswordLogin) => action.payload),
+    switchMap(loginData => {
+      return this.signInWithEmailAndPassword(loginData);
+    }),
+    switchMap(_ => of(new GetUser())),
+    catchError(error => of(new AuthError({ error: error.message })))
   );
 
   @Effect()
@@ -160,11 +161,10 @@ export class AuthEffects {
     return of(new GetUser());
   });
 
-  private signInWithEmailAndPassword(data) {
+  private signInWithEmailAndPassword(data): Observable<firebase.auth.UserCredential> {
     console.log('PasswordLogin', data);
-    return this.afAuth.auth.signInWithEmailAndPassword(
-      data.email,
-      data.password
+    return from(
+      this.afAuth.auth.signInWithEmailAndPassword(data.email, data.password)
     );
   }
 }
